d7c0be2a14846f73b383a3a2be72420659e13959
Patka879 <mysiak.patrycja@gmail.com>
2023-04-17 10:31:09 +0200

    Multidimensional arrays added

--- src/edu/sda/java/basics/Arrays.java
+++ src/edu/sda/java/basics/Arrays.java
@@ -20,5 +20,31 @@ public class Arrays {
         for (int i = 0; i< anotherTable.length; i++) {
             System.out.println("index: " + i + " " + anotherTable[i]);
         }
+
+        boolean[] boolTable = new boolean[4];
+        boolTable[0] = true;
+        boolTable[3] = true;
+
+        for (int i = 0; i < boolTable.length; i++) {
+            System.out.println("i = " + i + " " + boolTable[i]);
+        }
+
+        System.out.println("======");
+        String[][] multidimensional = new String[2][3];
+        multidimensional[0][0] = "Ala";
+        multidimensional[0][1] = "Ma";
+        multidimensional[0][2] = "Kota";
+
+        multidimensional[1][0] = "Kot";
+        multidimensional[1][1] = "Ma";
+        multidimensional[1][2] = "Ale";
+
+        for (int i = 0; i < multidimensional.length; i++) {
+            for (int j = 0; j < multidimensional[i].length; j++) {
+                System.out.print(multidimensional[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+
     }
 }
