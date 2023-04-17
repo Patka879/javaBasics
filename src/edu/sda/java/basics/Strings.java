093620e4bbbcdad1733bbec51eaf874909f49088
Patka879 <mysiak.patrycja@gmail.com>
2023-04-17 10:00:42 +0200

    Scanner classes added

--- src/edu/sda/java/basics/Strings.java
+++ src/edu/sda/java/basics/Strings.java
@@ -4,27 +4,44 @@ import java.util.Scanner;
 
 public class Strings {
     public static void main(String[] args) {
-        String text1 = "aaa";
-        String text2 = "aaa";
-        //both of above will point to same place in String Pool part of memory
+        canEnter();
+        calculator();
+        printName();
+    }
 
-        var text3 = "AAA"; //var keyword allows Java to guess what type of variable it is
+        private static void calculator() {
+            System.out.print("========");
+            System.out.println("Write first number");
+            Scanner scanner = new Scanner(System.in);
+            Integer number1 = scanner.nextInt();
+            System.out.println("Write a second number");
+            Integer number2 = scanner.nextInt();
+            System.out.println(number1 - number2);
+            System.out.println(number1 / number2);
+            System.out.println(number1 * number2);
+        }
 
-        System.out.print("Comparing strings:");
-        System.out.println(text1.equals(text2));
-        System.out.print("Comparing strings:");
-        System.out.println(text1.equals(text3));
+    private static void canEnter() {
+        System.out.println("Your height");
+        Scanner scanner = new Scanner(System.in);
+        Integer height = scanner.nextInt();
+        System.out.println("Your weight");
+        Integer weight = scanner.nextInt();
 
-        System.out.print("Comparing strings:");
-        System.out.println(text1.equalsIgnoreCase(text3));
+        if(height < 150 || weight < 180) {
+            System.out.println("Sorry we cannot let You in");
+        } else {
+            System.out.println("You can enter");
+        }
+    }
 
-        text3 = "BBB"; //this will not remove "AAA" string from String Pool -> it stays there, just that text3 returns new value
-        System.out.println(text3);
-
+    private static void printName() {
         System.out.print("========");
-        System.out.print("What is Your name?");
+        System.out.println("What is Your name?");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
-        System.out.print("Hello "  + name);
+        System.out.println("What is Your surname?");
+        String surname = scanner.nextLine();
+        System.out.print("Hello "  + name + " " + surname);
     }
 }
