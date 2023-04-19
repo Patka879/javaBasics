5b6455c815374e1bc95dfe8731c49f9367190ffd
Patka879 <mysiak.patrycja@gmail.com>
2023-04-19 20:58:01 +0200

    Calculator tests added

--- src/main/java/edu/sda/java/tdd/StringCalculator.java
+++ src/main/java/edu/sda/java/tdd/StringCalculator.java
@@ -3,10 +3,45 @@ package edu.sda.java.tdd;
 public class StringCalculator {
     public int sum(String argument) {
 
-        if (argument.equals("")) {
+        if (argument.isEmpty()) {
             return 0;
-        }
-        return Integer.parseInt(argument);
+        } else {
+            String delimiter = ",";
+            String inputNumbers = argument;
 
+            if (argument.startsWith("//")) {
+                int delimiterIndex = argument.indexOf("\n");
+                if (delimiterIndex != -1) {
+                    delimiter = argument.substring(2, delimiterIndex);
+                    inputNumbers = argument.substring(delimiterIndex + 1);
+                }
+            }
+
+            String[] nums = inputNumbers.split("[,\\n" + delimiter + "]");
+            int sum = 0;
+            StringBuilder negativeNumbers = new StringBuilder();
+            boolean hasNegative = false;
+            for (String num : nums) {
+                int n = Integer.parseInt(num);
+                if (n < 0) {
+                    if (negativeNumbers.length() > 0) {
+                        negativeNumbers.append(", ");
+                    }
+                    negativeNumbers.append(n);
+                    hasNegative = true;
+                }
+                sum += n;
+            }
+            if (hasNegative) {
+                throw new IllegalArgumentException("Negatives not allowed: " + negativeNumbers);
+            }
+            return sum;
+        }
     }
 }
+
+
+
+
+
+
--- src/test/java/edu/sda/java/tdd/StringCalculatorTest.java
+++ src/test/java/edu/sda/java/tdd/StringCalculatorTest.java
@@ -1,22 +1,20 @@
 package edu.sda.java.tdd;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
-    /**
-     * TDD - test driven development
-     *3 stages: RED, GREEN, REFACTOR
-     *
-     * 1. First most generic, not working code
-     * 2. Write test to check scenario
-     * 3. As scenario fails, fix the code
-     * 4. To add new feature go to 2.
-     */
-
     private StringCalculator calculator;
+
+    @BeforeAll
+    public static void mainSetup() {
+        /**
+         * method to run once for all tests before them
+         * I.E property entry data set
+         */
+    }
+
     @BeforeEach //Indicates that method should be run  before each test  in the class
     public void setup() {
         calculator = new StringCalculator();
@@ -40,4 +38,86 @@ class StringCalculatorTest {
         //then
         assertEquals(5, result,"Invalid value");
     }
+
+    @Test
+    public void checkIfCalcReturnsSumOfTwoPassedNumbers() {
+        //given
+        String entry = "3,4";
+        //when
+        var result = calculator.sum(entry);
+        //then
+        assertEquals(7,result,"Result is 7");
+    }
+
+    @Test
+    public void testUnknownAmountOfNumbers() {
+        //given
+        String entry = "1,2,3,4,5";
+        //when
+        var result = calculator.sum(entry);
+        //then
+        assertEquals(15, result, "Unknown number of Numbers is accepted, result is 15");
+    }
+
+    @Test
+    public void testSeparatingNumbersWithNewLinesAndCommas() {
+        //given
+        String entry = "1\n2,3,4";
+        //when
+        var result = calculator.sum(entry);
+        //then
+        assertEquals(10, result,"New lines are accepted as separator, result is 10");
+    }
+
+    @Test
+    public void checkIfCalculatorAcceptDelimiters() {
+        //given
+        String entries = "//;\n1;2";
+        //when
+        var result = calculator.sum(entries);
+        //then
+        assertEquals(3,result,"Different delimiters accepted, result is 3");
+    }
+    @Test
+    public void testPassingNegativeNumbers() {
+        //given
+        String entries = "-5";
+        //when
+        var result = calculator.sum(entries);
+        //then
+        assertEquals("Negatives not allowed: -5", result,"Returns message and entries");
+    }
+
+    @AfterEach // run after each test
+    public void destroy() {
+        calculator.hashCode(); //may be useful to return to previous app state
+    }
+
+    @AfterAll
+    public static void finalCleanup() {
+        /**
+         * run once after all test
+         */
+    }
 }
+
+
+/**
+ * TDD - test driven development
+ *3 stages: RED, GREEN, REFACTOR
+ *
+ * 1. First most generic, not working code
+ * 2. Write test to check scenario
+ * 3. As scenario fails, fix the code
+ * 4. To add new feature go to 2.
+ */
+/**
+ * Co mamy wiedzieć o testach?
+ * - Jak odpalać testy?
+ * - Anotacja @Test żeby metoda była testem
+ * - Sprawdzamy jakimś assertem (assert.equals sprawdza każda pojedynczą wartość,
+ *   zawartość tabel)
+ * - Piszemy testy z podziałem: given, when, then
+ * - Podejście tdd (najpierw test, potem kod właściwy ktory będzie nam zwracał
+ *   zaliczony test)
+ */
